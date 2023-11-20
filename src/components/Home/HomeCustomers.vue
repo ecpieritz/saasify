4fcd7192f1337b2a575ecce8a8fc320755012828
ecpieritz <ecpieritz@gmail.com>
2023-11-20 12:44:52 -0300

    Added HomeCustomers

---
+++ src/components/Home/HomeCustomers.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="sf-home__customers">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <img src="@/assets/imgs/sf-logo__airbnb.png" alt="">
+        </div>
+        <div class="col">
+          <img src="@/assets/imgs/sf-logo__amazon.png" alt="">
+        </div>
+        <div class="col">
+          <img src="@/assets/imgs/sf-logo__google.png" alt="">
+        </div>
+        <div class="col">
+          <img src="@/assets/imgs/sf-logo__netflix.png" alt="">
+        </div>
+        <div class="col">
+          <img src="@/assets/imgs/sf-logo__slack.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HomeCustomers",
+  props: {
+  },
+  components: {},
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <template>
   <main>
     <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')" />
-    <h1>Home</h1>
+    <HomeCustomers />
   </main>
 </template>
 
 <script>
 import Banner from '@/components/Banner.vue'
+import HomeCustomers from '@/components/Home/HomeCustomers.vue'
 export default {
   name: 'HomeView',
   components: {
     Banner,
+    HomeCustomers
   }
 }
 </script>
