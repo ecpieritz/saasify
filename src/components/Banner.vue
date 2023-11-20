ddad153d0c6c523f24152647bf7168c585cdfb4f
ecpieritz <ecpieritz@gmail.com>
2023-11-20 11:46:18 -0300

    Added Home Banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="sf-banner">
+    <div class="container-fluid">
+      <div class="sf-banner__texts">
+        <h1>The fastest way for startups to do any analysis</h1>
+        <p>
+          Equals is the only spreadsheet with built-in connections to any database,
+          versioning, and collaboration.
+        </p>
+      </div>
+      <div class="sf-banner__form input-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter your email"
+          aria-label="Enter your email"
+          aria-describedby="bannerHomeBtn"
+        />
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button" id="bannerHomeBtn">
+            Start for free
+          </button>
+        </div>
+      </div>
+      <img class="sf-banner__img" :src="img" alt="banner image" />
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "BannerComp",
+  props: {
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {},
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <main>
+    <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')" />
     <h1>Home</h1>
   </main>
 </template>
 
 <script>
-
+import Banner from '@/components/Banner.vue'
 export default {
   name: 'HomeView',
   components: {
+    Banner,
   }
 }
 </script>
