c1d1dd4ee24f9ed8b7462f86a7c34e11d97cd9ef
ecpieritz <ecpieritz@gmail.com>
2023-11-20 11:08:49 -0300

    Added Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="sf-navbar">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4 sf-navbar__logo">
+          <router-link to="/">SaaSify</router-link>
+        </div>
+
+        <div class="col-md-8 sf-navbar__nav">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <router-link to="/solutions">Solutions</router-link>
+          <router-link to="/customers">Customers</router-link>
+          <router-link to="/contact">Contact</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  components: {
+  }
+}
+</script>
