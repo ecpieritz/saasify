a6818a86072b872d3aabfb3d039107f4f955651e
ecpieritz <ecpieritz@gmail.com>
2023-11-21 11:57:37 -0300

    Added HomePlatform

---
+++ src/components/Home/HomePlatform.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="sf-home__platform">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 sf-home__platform__text">
+          <h2>Powerful and easy to use SaaS platform</h2>
+          <p>Use marketing automation to identify hot leads and email your sales team telling them to follow up. Nurture and contact leads at the opportune time to maximize.</p>
+          <a href="/contact" class="sf-btn gradient">Get Started</a>
+        </div>
+        <div class="col-md-6 sf-home__platform__img">
+          <img src="@/assets/imgs/sf-home__platform.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HomePlatform",
+  props: {},
+  components: {},
+};
+</script>
