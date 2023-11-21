397f5dd5ecbd73d6cb4f31017c722bfad0457e25
ecpieritz <ecpieritz@gmail.com>
2023-11-21 12:09:51 -0300

    Added sf-btn gradient

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')" />
     <HomeCustomers />
     <HomeServices />
+    <HomePlatform />
   </main>
 </template>
 
@@ -10,12 +11,15 @@
 import Banner from '@/components/Banner.vue'
 import HomeCustomers from '@/components/Home/HomeCustomers.vue'
 import HomeServices from '@/components/Home/HomeServices.vue'
+import HomePlatform from '@/components/Home/HomePlatform.vue'
+
 export default {
   name: 'HomeView',
   components: {
     Banner,
     HomeCustomers,
-    HomeServices
+    HomeServices,
+    HomePlatform
   }
 }
 </script>
