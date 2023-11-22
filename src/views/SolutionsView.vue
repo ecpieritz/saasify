3ca1dd2c5a5f0bbbf5e794855c00614da2c44a4c
ecpieritz <ecpieritz@gmail.com>
2023-11-22 12:04:32 -0300

    Added Solutions page

---
+++ src/views/SolutionsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')" />
+    Solutions
+  </main>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue'
+
+export default {
+  name: 'SolutionsView',
+  components: {
+    Banner,
+  }
+}
+</script>
