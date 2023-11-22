8747bf89362e1109d7aaf0d8ee20edf0e1e0c266
ecpieritz <ecpieritz@gmail.com>
2023-11-22 12:04:13 -0300

    Added Customers page

---
+++ src/views/CustomersView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')" />
+    Customers
+  </main>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue'
+
+export default {
+  name: 'CustomersView',
+  components: {
+    Banner,
+  }
+}
+</script>
