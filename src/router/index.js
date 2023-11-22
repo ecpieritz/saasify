5ef476128c2ac182cff836efaba7aa1c0b9b18e9
ecpieritz <ecpieritz@gmail.com>
2023-11-22 12:04:54 -0300

    Added pages routes

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,23 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/solutions',
+    name: 'solutions',
+    component: () => import('../views/SolutionsView.vue')
+  },
+  {
+    path: '/customers',
+    name: 'customers',
+    component: () => import('../views/CustomersView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/ContactView.vue')
+  },
 ]
 
 const router = createRouter({
