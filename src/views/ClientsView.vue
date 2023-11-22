6f8a1db48007e418b522dd7594521f424e3754ff
ecpieritz <ecpieritz@gmail.com>
2023-11-22 12:50:20 -0300

    Added Clients banner

--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,6 +1,16 @@
 <template>
   <main>
-    <Banner :img="require('@/assets/imgs/sf-banner__clients-img.jpg')"> </Banner>
+    <Banner :img="require('@/assets/imgs/sf-banner__clients-img.jpg')">
+      <slot>
+        <div class="sf-banner__texts">
+          <h1>The CLIENTS we are proud to work with</h1>
+          <h4>
+            Equals is the only spreadsheet with built-in connections to any database,
+            versioning, and collaboration.
+          </h4>
+        </div>
+      </slot>
+    </Banner>
   </main>
 </template>
 
