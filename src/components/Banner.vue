1ac7615b5c01f13648004354ada25ac977bda469
ecpieritz <ecpieritz@gmail.com>
2023-11-22 12:25:40 -0300

    Added slot into Banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,27 +1,7 @@
 <template>
   <div class="sf-banner">
     <div class="container-fluid">
-      <div class="sf-banner__texts">
-        <h1>The fastest way for startups to do any analysis</h1>
-        <h4>
-          Equals is the only spreadsheet with built-in connections to any database,
-          versioning, and collaboration.
-        </h4>
-      </div>
-      <div class="sf-banner__form input-group">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Enter your email"
-          aria-label="Enter your email"
-          aria-describedby="bannerHomeBtn"
-        />
-        <div class="input-group-append">
-          <button class="btn" type="button" id="bannerHomeBtn">
-            Start for free
-          </button>
-        </div>
-      </div>
+      <slot />
       <img class="sf-banner__img" :src="img" alt="banner image" />
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,28 @@
 <template>
   <main>
-    <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')" />
+    <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')">
+      <slot>
+        <div class="sf-banner__texts">
+          <h1>The fastest way for startups to do any analysis</h1>
+          <h4>
+            Equals is the only spreadsheet with built-in connections to any database,
+            versioning, and collaboration.
+          </h4>
+        </div>
+        <div class="sf-banner__form input-group">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your email"
+            aria-label="Enter your email"
+            aria-describedby="bannerHomeBtn"
+          />
+          <div class="input-group-append">
+            <button class="btn" type="button" id="bannerHomeBtn">Start for free</button>
+          </div>
+        </div>
+      </slot>
+    </Banner>
     <HomeCustomers />
     <HomeServices />
     <HomePlatform />
@@ -10,22 +32,22 @@
 </template>
 
 <script>
-import Banner from '@/components/Banner.vue'
-import HomeCustomers from '@/components/Home/HomeCustomers.vue'
-import HomeServices from '@/components/Home/HomeServices.vue'
-import HomePlatform from '@/components/Home/HomePlatform.vue'
-import HomeWork from '@/components/Home/HomeWork.vue'
-import HomeTestimonials from '@/components/Home/HomeTestimonials.vue'
+import Banner from "@/components/Banner.vue";
+import HomeCustomers from "@/components/Home/HomeCustomers.vue";
+import HomeServices from "@/components/Home/HomeServices.vue";
+import HomePlatform from "@/components/Home/HomePlatform.vue";
+import HomeWork from "@/components/Home/HomeWork.vue";
+import HomeTestimonials from "@/components/Home/HomeTestimonials.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     Banner,
     HomeCustomers,
     HomeServices,
     HomePlatform,
     HomeWork,
-    HomeTestimonials
-  }
-}
+    HomeTestimonials,
+  },
+};
 </script>
