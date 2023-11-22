ddb6516a11212f0ccad18677f5fc9e04d19e8b8d
ecpieritz <ecpieritz@gmail.com>
2023-11-22 12:03:44 -0300

    Added About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>About</h1>
-  </div>
+  <main>
+    <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')" />
+    About
+  </main>
 </template>
+
+<script>
+import Banner from '@/components/Banner.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    Banner,
+  }
+}
+</script>
