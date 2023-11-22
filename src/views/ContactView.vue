bf72f9c0913b4608a484efca55123c1c521aa629
ecpieritz <ecpieritz@gmail.com>
2023-11-22 14:50:08 -0300

    Added ContactForm scss

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,17 +1,26 @@
 <template>
-  <main>
-    <Banner :img="require('@/assets/imgs/sf-banner__home-img.png')" />
-    Contact
+  <main class="mb-5">
+    <Banner :img="require('@/assets/imgs/sf-banner__clients-img.jpg')" class="pb-5">
+      <slot>
+        <div class="sf-banner__texts">
+          <h1>Get in touch to find out more about our products</h1>
+
+          <ContactForm />
+        </div>
+      </slot>
+    </Banner>
   </main>
 </template>
 
 <script>
-import Banner from '@/components/Banner.vue'
+import Banner from "@/components/Banner.vue";
+import ContactForm from "@/components/Contact/ContactForm.vue";
 
 export default {
-  name: 'BannerView',
+  name: "BannerView",
   components: {
     Banner,
-  }
-}
+    ContactForm,
+  },
+};
 </script>
