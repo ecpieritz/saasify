8c7012cf383d21e017b6e90a01ec4d686de3cec3
ecpieritz <ecpieritz@gmail.com>
2023-11-24 12:29:46 -0300

    Added ClientsItem

---
+++ src/components/Clients/ClientsItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-md-4 sf-clients__cards__item">
+    <div class="row">
+      <div class="col-4 sf-clients__cards__item__client">
+        <img :src="img" :alt="client" />
+      </div>
+      <div class="col-8 sf-clients__cards__item__text">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ClientsItem",
+  props: {
+    img: {
+      type: String,
+    },
+    client: {
+      type: String,
+    },
+  },
+};
+</script>
