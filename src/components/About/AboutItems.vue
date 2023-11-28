3073b857297e48e71dcf050398912cc2d7fac1c4
ecpieritz <ecpieritz@gmail.com>
2023-11-28 11:39:05 -0300

    Added About Items base

---
+++ src/components/About/AboutItems.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sf-about__content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 sf-about__content__item">
+          <div class="row">
+            <div class="col-md-6"></div>
+            <div class="col-md-6"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutItems",
+  components: {},
+};
+</script>
